2718414250c661e443beab4d3c59f1feaa4c558a
Paulo Gonçalves <paulo.programmator@gmail.com>
2022-12-12 13:03:39 -0300

    animação de loading usando java script

--- script.js
+++ script.js
@@ -1,57 +1,61 @@
 function previewImage() {
-    var image = document.querySelector("input[name=image]").files[0];
-    var preview = document.querySelector("img");
-    var reader = new FileReader();
-    reader.onloadend = () => {
-      preview.src = reader.result;
-    };
+  var image = document.querySelector("input[name=image]").files[0];
+  var preview = document.querySelector("img");
+  var reader = new FileReader();
+  reader.onloadend = () => {
+    preview.src = reader.result;
+  };
 
-    if (image) {
-      reader.readAsDataURL(image);
-      console.log(image);
-      document.getElementById("location-src").innerText = image.name;
-    } else {
-      preview.src = "";
-    }
+  if (image) {
+    reader.readAsDataURL(image);
+    console.log(image);
+    document.getElementById("location-src").innerText = image.name;
+  } else {
+    preview.src = "";
   }
+}
 
-  // More API functions here:
-    // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
+function exibir() {
+  document.getElementById("loading").style.display = "block";
+}
 
-    // the link to your model provided by Teachable Machine export panel
-    const URL = "https://teachablemachine.withgoogle.com/models/6ZmYDctzE/";
+function ocultar() {
+  document.getElementById("loading").style.display = "none";
+}
 
-    let model, webcam, labelContainer, maxPredictions;
+// More API functions here:
+// https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
-    // Load the image model and setup the webcam
-    async function init()  {
-        
-        const modelURL = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
-        model = await tmImage.load(modelURL, metadataURL);
-        predict();
-        document.getElementById("location-src").innerText = "Envie uma Foto da sua Cerveja( Brahma , Skol ou Antarctica)";
-           
-      }
+// the link to your model provided by Teachable Machine export panel
+const URL = "https://teachablemachine.withgoogle.com/models/6ZmYDctzE/";
 
-    async function loop() {
-        await predict();
-    }
+let model, webcam, labelContainer, maxPredictions;
 
-    // run the webcam image through the image model
-    async function predict() {
+// Load the image model and setup the webcam
+async function init() {
+  exibir();
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
+  model = await tmImage.load(modelURL, metadataURL);
+  predict();
+  document.getElementById("location-src").innerText =
+    "Envie uma Foto da sua Cerveja( Brahma , Skol ou Antarctica)";
+}
 
-        const predictions = await model.predict(
-            document.getElementById("preview")
-          );
-          
-          //busca o maior valor probabilistico da classificação de uma imagem
-          var max = predictions.reduce(function (prev, current) {
-            return prev.probability > current.probability ? prev : current;
-          });
+async function loop() {
+  await predict();
+}
 
-          document.getElementById("label-container").innerHTML = max.className;
-          console.log(max.className);
-    }
+// run the webcam image through the image model
+async function predict() {
+  const predictions = await model.predict(document.getElementById("preview"));
 
-  
+  //busca o maior valor probabilistico da classificação de uma imagem
+  var max = predictions.reduce(function (prev, current) {
+    return prev.probability > current.probability ? prev : current;
+  });
+
+  document.getElementById("label-container").innerHTML = max.className;
+  console.log(max.className);
+  ocultar();
+}
